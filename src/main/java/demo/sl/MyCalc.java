a33959e22a415ddeba51bbadf06f8425ba71d45e
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-05-03 21:41:16 +0530

    div() added

--- src/main/java/demo/sl/MyCalc.java
+++ src/main/java/demo/sl/MyCalc.java
@@ -13,6 +13,11 @@ public class MyCalc {
 	{
 		return a*b;
 	}
+	public int div(int a, int b)
+	{
+		return a/b;
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

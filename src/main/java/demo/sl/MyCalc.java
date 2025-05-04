a3c5978341b132a4fa8e71725fe0c0d01f41ea68
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-05-04 20:40:14 +0530

    Greet added

--- src/main/java/demo/sl/MyCalc.java
+++ src/main/java/demo/sl/MyCalc.java
@@ -21,6 +21,10 @@ public class MyCalc {
 	{
 		return a%b;
 	}
+	public void greet()
+	{
+		System.out.println("Welcome to Jenkinsfile ");
+	}
 
 
 	public static void main(String[] args) {
@@ -32,6 +36,7 @@ public class MyCalc {
 		System.out.println("Mul is : "+calc.mul(20,10));
 		System.out.println("Div is : "+calc.div(20,10));
 		System.out.println("Mod is : "+calc.mod(20,10));
+		calc.greet();
 	}
 
 }
